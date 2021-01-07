46e57d4833e3bad35fe6d4d4a1de5bc9e0f5f589
Darren Garcia <darrengarcia@Darrens-MacBook-Pro.local>
2021-01-06 22:23:52 -0600

    help from tha padawan

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const { v4: uuidv4 } = require('uuid');
 const fs = require("fs");
-const dbJSON = require("../02-Homework/Develop/db/db.json"); // change later
+let dbJSON = require("./db/db.json"); // change later
 const path = require("path");
 
 // Sets up the Express App
@@ -24,17 +24,17 @@ app.use(express.static("public"));
 // Basic route that sends the user first to the AJAX Page
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname,"/Develop/public/index.html"));
+  res.sendFile(path.join(__dirname,"/public/index.html"));
 });
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname,"/Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname,"/public/notes.html"));
 });
 app.get("/api/notes", function(req, res) {
   res.json(dbJSON);
 });
 
 
-app.post("/note", function(req, res) {
+app.post("/api/notes", function(req, res) {
   // Validate request body
   if(!req.body.title) {
     return res.json({error: "Missing required title"});
@@ -45,11 +45,12 @@ app.post("/note", function(req, res) {
 
   // Push note to dbJSON array - saves data in memory
   dbJSON.push(note);
+  console.log(dbJSON);
 
   // Saves data to file by persisting in memory variable dbJSON to db.json file.
   // This is needed because when we turn off server we loose all memory data like pbJSON variable.
   // Saving to file allows us to read previous notes (before server was shutdown) from file.
-  fs.writeFile(path.join(__dirname, "/db.json"), JSON.stringify(dbJSON), (err) => { 
+  fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(dbJSON), (err) => { 
     if (err) {
       return res.json({error: "Error writing to file"});
     }
@@ -69,7 +70,7 @@ app.delete("/api/notes/:id",function(req,res){
   }
 });
 console.log(dbJSON);
-fs.writeFile("../02-Homework/Develop/db/db.json",JSON.stringify(dbJSON),function(error){
+fs.writeFile(__dirname + "/db/db.json",JSON.stringify(dbJSON),function(error){
   if(error)
     throw error;
     return res.json(true);
